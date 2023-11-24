507eb867e169f7bc41afa0c3f1353ec80d5885b8
Alphacode@77 <emmanueloteng.k@gmail.com>
2023-11-24 11:36:00 +0000

    First Commit

---
+++ New Project/Phd/js/phd-course.js
@@ -0,0 +1,194 @@
+
+const data = [
+    
+  { 
+    "coursecode":"FASC 701",
+    "title": "Science and Society (Faculty Wide)",
+    "data":  "This course will enable students gain insights on the practice of science as a discipline including major scientific concepts like inductivism are examined as well as the history of science and science itself, an overview of current approaches to research and an understanding of research partnerships, networks and appropriate methods of communicating science depending on the audience. The aim of the course is to help students to fit their research to relevant trends and directions for national development. Course content will cover topics such as the basis for the scientific method; conceptual frameworks; the philosophy of science; ethics in research; pure versus applied science debates; approaches to research; science for development and the merit of broader impact criteria; north south/south south collaboration and partnerships; research networks; communicating science to the policy make, lay audience and to the media"
+  }, 
+  
+  { 
+    "coursecode":"FASC 700",
+    "title": "Special Topics in Science",
+    "data":"The course examines historical and contemporary issues in science, relating to the student’s area of specialization and relevance.  Such topics are expected to challenge the students into exploring current and relevant research trends/discoveries in scientific approaches. The course will enable students explore scientific knowledge in modern science, and add on to their depth of information in their chosen areas of specialty. It is expected that, the course will complement other courses on the PhD flagship of the various departments in the Sciences and elsewhere. Additionally, it will expose students to current trends of presentations, and foster stronger confidence-building attitude that will enable enhanced international academic competitive spirit.",
+  }, 
+  
+  { 
+    "coursecode":"CSCD 721",
+    "title": "Grand Research Challenges in Computer Science",
+    "data":  "Wireless communication has become a ubiquitous part of modern life, from global cellular telephone systems to local and even personal-area networks. This course provides an in-depth introduction to digital mobile wireless networks, illustrating theoretical underpinnings with a wide range of real-world examples. The course will review propagation phenomena, modulation techniques, multiple access schemes, and coding techniques, air interface design, wireless network planning and operations, GSM, TDMA and CDMA technologies"
+  }, 
+  
+  { 
+    "coursecode":"CSCD 723",
+    "title": "Emerging Database Models",
+    "data":  "This course examines the science underpinning computer communications, such as the basic architectural principles of computer networking and specifically how the Internet works today. Covered topics include network software and hardware, reference model and network layers including Y-comm model, data representation, how errors in transmission can be detected and dealt with, the way information is routed over a large network, how congestion can be avoided, aspects of network security, and socket programming"
+  }, 
+  
+  { 
+    "coursecode":"CSCD 725",
+    "title": "Research in Bioinformatics and Computational Biology",
+    "data":  "The aim of this course unit is to survey the research landscape of advanced DBMS systems with a view to understanding how DBMS research is responding to challenges arising from new software architectures, new kinds of data resource and new computational fabrics. Topics include: Architecture, Components: The Classical Case and Variations; The Relational Case: Data Models, Databases, Languages; Query Processing (1): Overview, Equivalence-Based Rewriting; Query Processing (2): Algorithms, Evaluation Strategies, Cost-Based Optimization; Parallel DBMSs [2]; Distributed DBMSs and Dataspaces; Massively-Parallel/Massively-Distributed Data Processing; NoSQL and Cloud Database; Stream Data Management; Sensor Network Data Management"
+  }, 
+  
+  { 
+    "coursecode":"CSCD 727",
+    "title": "Ubiquitous Computing",
+    "data":  "The advanced operating system course examines the structural aspects of operating system and how these provide support for general purpose, embedded, and real-time operating environments. Topics include survey of early systems, structural design of operating system including process model, inter-process communication, synchronization mechanisms, resource management, CPU scheduling, I/O scheduling, file systems, virtual machines, protection issues, implementation issues of modern operating systems, performance analysis, deadlock detection, recovery and avoidance, operating system for distributed and current systems, review of current research in operating systems"
+  }, 
+  
+  { 
+    "coursecode":"CSCD 722",
+    "title": "New Developments in Cyber Security and Networks",
+    "data":  "The human computer interaction course provides key approaches to the design, development, and evaluation of human-computer interfaces, with an emphasis on usability, interaction paradigms, computer-mediated human activities, and implications to society. Topics include foundation of HCI and technologies, HCI paradigms and history, nature of human computer interaction, use and context of computers, human characteristics including human information processing, language and communication interaction, and ergonomics, computer system and interface architecture, development process including design approaches, implementation techniques and tools, evaluation techniques, user interface software and error handling, multimedia systems, interaction design for new environment"
+  }, 
+  
+  { 
+    "coursecode":"FASC 702 ",
+    "title": "Advanced Quantitative Research Methods",
+    "data":  " The course will serve as a step up for students who need to add up to their knowledge in quantitative methods of research techniques and analyses.Topics to be covered include: Sampling distributions and hypothesis testing.Sample size determination. Categorical data and chi-square, Non parametric tests.Principles of Design of Experiments.Analysis of variance and its assumptions. Experiments with single and multiple factors. Orthogonal and multiple Comparisons. Completely Randomized, Randomized Complete Block, repeated measures, cross over and Latin square designs. Nested designs.  Fixed, random and mixed effects models. Factorial designs. Confounding. Fractional factorial designs. Split plot designs. Incomplete block designs. Analysis of covariance. Regression models:  basic concepts; Regression Model Diagnostics. Categorical data analysis. Logistic regression, univariate and multivariate. Confounding and collinearity in logistic regression.Model selection in logistic regression"
+  }, 
+  
+  { 
+    "coursecode":"CSCD 724",
+    "title": "New Developments in Computer Architecture",
+    "data":  "This is a course which highlights the interplay of algebra and geometry.  It includes topics such as: polar coordinates; conic sections. Complex numbers, Argand diagram, DeMoivre's theorem, roots of unity. Algebra of matrices and determinants, linear transformations. Transformations of the complex plane.  Sketching polar curves and some coordinate geometry in 3 dimensions. Vector product and triple products.  "
+  }, 
+  
+  { 
+    "coursecode":"CSCD 726", 
+    "title": "Next Generation Wireless Systems and Networks",
+    "data":  "On starting their programme of study, students will be guided to develop a proposal for their research thesis or to define a problem for their project work. The supervisor will recommend up to three journal papers or books for the student to study and use that as a basis for developing a proposal. The full proposal should (1) define a problem of interest to the student, (2) summarize past work, (3) outline work on an approach to solving the problem and (4) define the scope and limitations of the work. This proposal shall form the basis of the work to be carried out in the final year. It is important to note that this is still just a proposal and there will be no requirement on the candidate to continue working in the proposed area. The candidate, with the approval of their supervisor, may modify the scope and direction of research at any time"
+  }, 
+  
+  { 
+    "coursecode":"CSCD 716",
+    "title": "New Directions in Artificial Intelligence",
+    "data":  " In this course students will be encouraged to focus on some of the fundamental techniques for solving AI problems. Areas to be explored include vision, robotics, probabilistic reasoning, computational logic, machine learning, multi-agent systems, and natural language processing. Some other key issues that students will be encouraged to explore are Turing Test and the feasibility that such a test can be passed in the near or distant future, informed search, greedy search, heuristic function, Constraint satisfaction problems and others"
+  }, 
+  
+  { 
+    "coursecode":"CSCD 728",
+    "title": "Research Issues in Social Networks",
+    "data":  "" 
+    
+  }, 
+  
+  { 
+    "coursecode":"CSCD 732",
+    "title": "Advances in High Performance Computing",
+    "data":  "High-level processing for image understanding and high-level vision. Data structures, algorithms, and modelling. Low-level representation, basic pattern-recognition and image-analysis techniques, segmentation, color, texture and motion analysis, and representation of 2-D and 3-D shape. Applications for content-based image retrieval, digital libraries, and interpretation of satellite imagery. Topics  include: Introduction to Computer Vision; Low-level image processing ;Binary image analysis ; Segmentation ;Texture; Color; Pattern recognition techniques; Feature Extraction; Matching; Motion; Applications: Content-based image retrieval ; Applications: Biometrics"
+  }, 
+  
+  { 
+    "coursecode":"FASC 710",
+    "title": "Teaching Science at the Tertiary Level",
+    "data":  "It is anticipated that many of the students who go through the PhD programme in the Sciences may nurse special interest in teaching and academia. Focusing on group discussions, this course is expected to equip students with the requisite knowledge in overall management of students at the tertiary level. The course will focus on teaching the methodologies and techniques in handling Science-teaching at the undergraduate level. Topics such as laboratory supervision and safety, grading issues, special needs students, lecturing and tutoring techniques, examination preparation, teacher/student relationship, tertiary education management, will be discussed through reading, class/group discussions as well as presentations."
+  }, 
+  
+  { 
+    "coursecode":"CSCD 710",
+    "title": "Seminar I (Research Proposal)",
+    "data":  "This course will focus on developing the original synopsis or other agreed topic into a full PhD proposal. The ultimate aim of this seminar is to enable students to obtain feedback from the supervisory team on what directions to take to successfully complete a quality dissertation. The full proposal should (1) define a problem of interest to the student, (2) summarize past work, (3) outline work on an approach to solving the problem and (4) present initial results obtained. Ideally the subject matter of this proposal should be publishable in conference proceedings ranked at C or above in the ERA list"
+  }, 
+  
+  { 
+    "coursecode":"CSCD 720 ",
+    "title": "Seminar II (Experiential research learning)",
+    "data":  "This seminar will provide candidates with the opportunity to demonstrate knowledge they have gained through experience. Opportunities will be provided, as far is possible, to enable candidates gained industrially or commercially relevant experience and to demonstrate that through their work. "
+  }, 
+  
+  { 
+    "coursecode":"CSCD 730",
+    "title": "PhD Thesis",
+    "data":  "This seminar, in the third year of the investigation, provides an opportunity for the supervisors to check that work is progressing satisfactorily and that candidate is on course for a successful completion. Candidates will be required to report on the progress of their work. For this an internal examiner shall be appointed. Candidates must see this as preparation for the real viva in the final year. The assessment result for this course shall be classified as either satisfactory or unsatisfactory. In each case student shall be provided guidance on what to do next"
+  },
+  { 
+    "coursecode":"CSCD 740",
+    "title": "Seminar IV (Research Results)",
+    "data":  "This is the final part of the work, a public presentation of the major findings of the research. The presentation will be subject to scrutiny by an external examiner, an internal examiner, the supervisory team and the general public. The aim is to ensure that the work is of an acceptably high standard deserving of an award of the degree of doctor of philosophy"
+  },
+  { 
+    "coursecode":"CSCD 700",
+    "title": "PhD Thesis",
+    "data":  "The candidate, working closely under the supervision of their advisor shall present a thesis for assessment. The candidate’s thesis will be the final report on the work done over the period of research investigation. It shall outline the problem, the motivation for the work, previous work done, methodology and present the findings of the current research and any implications for future directions. The candidate must demonstrate in this thesis their fundamental contribution to the area of study. "
+  },
+
+]
+
+
+const searchField = document.querySelector('.search-course');
+const filter = 'research';
+//variables for holding outputs of first elements of each result
+let resultDiv = document.querySelector('.results');
+let courseTitle = document.querySelector('.course-title');
+let courseCode = document.querySelector('.coursecode');
+let discription = document.querySelector('.discription');
+
+searchField.addEventListener("keydown", () => {
+  
+  searchDisplay(searchField.value);
+});
+
+
+
+const searchDisplay = filter => {
+let filteredCourse = performFilter(filter);
+displayResults(filteredCourse);
+} 
+const displayDiscription = (id) => {
+  let searchValueField = ''
+  searchField.value? searchValueField = searchField.value : searchValueField = filter; 
+  let filteredCourse = performFilter(searchValueField);
+  courseTitle.textContent = filteredCourse[id].title;
+  courseCode.textContent = filteredCourse[id].coursecode;
+  discription.textContent = filteredCourse[id].data;
+}
+
+const performFilter = (filterBy) => {
+  filterBy = filterBy.toLocaleLowerCase();
+  return data.filter((datum) =>
+      datum.title.toLocaleLowerCase().indexOf(filterBy) !== -1);
+};
+
+let filteredCourse = performFilter(filter);
+
+courseTitle.textContent = filteredCourse[0].title;
+courseCode.textContent=filteredCourse[0].coursecode;
+discription.textContent = filteredCourse[0].data;
+
+const displayResults = filteredCourse => {
+  resultDiv.innerHTML='';
+  let newTable = document.createElement('table');
+  filteredCourse.forEach(course => {
+    if (newTable.childElementCount > 13) {
+        return;
+      }
+      console.log(newTable.childElementCount);
+      let tableRow = document.createElement('tr');
+      let courseCodeItem = document.createElement('td');
+      let courseCodeAnchorTag = document.createElement('a');
+      let courseTitleItem = document.createElement('td');
+      let courseTitleAnchorTag = document.createElement('a');
+      courseCodeAnchorTag.textContent = course.coursecode;
+      courseTitleAnchorTag.textContent = course.title;
+      courseCodeItem.style.paddingRight = '15px';
+      courseCodeAnchorTag.href = `javascript:displayDiscription(${newTable.childElementCount})`;
+      courseTitleAnchorTag.href = `javascript:displayDiscription(${newTable.childElementCount})`;
+      courseTitleAnchorTag.onclick = function() { displayDiscription();};
+      courseCodeAnchorTag.onclick = function() { displayDiscription();};
+      courseCodeItem.appendChild(courseCodeAnchorTag);
+      courseTitleItem.appendChild(courseTitleAnchorTag);
+      tableRow.appendChild(courseCodeItem);
+      tableRow.appendChild(courseTitleItem);
+
+      newTable.appendChild(tableRow);
+      
+  });
+
+  resultDiv.appendChild(newTable);
+}
+
+
+searchField.value ? searchDisplay(searchField.value) : displayResults(filteredCourse);
+
